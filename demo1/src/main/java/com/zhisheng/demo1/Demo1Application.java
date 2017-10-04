1229d635bd4aa87ce7af2543c0554005d0a16887
zhisheng17 <1041218129@qq.com>
2017-10-04 11:08:59 +0800

    设置port

--- demo1/src/main/java/com/zhisheng/demo1/Demo1Application.java
+++ demo1/src/main/java/com/zhisheng/demo1/Demo1Application.java
@@ -3,13 +3,21 @@ package com.zhisheng.demo1;
 import com.zhisheng.demo1.domain.ConfigBean;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.embedded.ConfigurableEmbeddedServletContainer;
+import org.springframework.boot.context.embedded.EmbeddedServletContainerCustomizer;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication	// same as @Configuration @EnableAutoConfiguration @ComponentScan
 @EnableConfigurationProperties(ConfigBean.class)
-public class Demo1Application {
+public class Demo1Application implements EmbeddedServletContainerCustomizer
+{
 
 	public static void main(String[] args) {
 		SpringApplication.run(Demo1Application.class, args);
+	}
+
+	@Override
+	public void customize(ConfigurableEmbeddedServletContainer container) {
+		container.setPort(8082);    //设置端口为 8082
 	}
 }
